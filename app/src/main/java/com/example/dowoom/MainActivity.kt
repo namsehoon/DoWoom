381d9b68256cd074ceb8973de2b1c88324dae71c
namsehoon <skatpgnsx@naver.com>
2022-02-09 12:20:41 +0900

    databinding 연결 및 xml : viewpager2, tablayout 구성

--- app/src/main/java/com/example/dowoom/MainActivity.kt
+++ app/src/main/java/com/example/dowoom/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.dowoom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.dowoom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    //데이터바인딩
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //초기화
+        initialized()
+
+
+    }
+
+    fun initialized() {
+        //바인딩
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //main.xml 정의된 이름.
+        binding.activity = this
+
     }
 }
